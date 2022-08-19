58930fdeedf4329a9baa673883fc5ccbc3f090cb
Francisco Stenico <francisco.stenico@gmail.com>
2022-08-19 20:50:44 -0300

    Refact (type): JSX|JS >>> TSX|TS
    - Alteração de todas as extensões de JSX para TSX & JS para TS

--- src/validators/createTech.js
+++
@@ -1,7 +0,0 @@
-import * as yup from "yup";
-
-const createTechSchema = yup.object().shape({
-  title: yup.string().required("Adicione uma nova tecnologia"),
-});
-
-export default createTechSchema;
