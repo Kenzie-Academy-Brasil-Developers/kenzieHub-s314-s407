94bea5e380afcb2199f4131e67b121fb01acc144
Francisco Stenico <francisco.stenico@gmail.com>
2022-08-19 21:17:32 -0300

    Refact(src): Tipagem, teste e debug da aplicação
    - Após conclusão do projeto, foi feito o teste para verificar se a
    aplicação foi quebrada em algum ambito. Os seguintes testes foram
    realizados e concluídos com êxito:
        > Alteração de rota: Login > Register;
        > Falha na requisição de criação de usuário;
        > Sucesso na requisição de criação de usuário;
        > Alteração de rota: Register > Login;
        > Falha na requisição de Login;
        > Sucesso na requisição de Login;
        > Alteração de rota (login): Login > Dashboard;
        > Falha na requisição de Criação de tecnologia;
        > Sucesso na requisição de Criação de tecnologia;
        > Falha na requisição de Edição de tecnologia;
        > Sucesso na requisição de Edição de tecnologia;
        > Sucesso na requisição de Exclusão de tecnologia;
        > Alteração de rota (logout): Dashboard > Login;
    
        OBS: Até o ponto, o teste de rota segura FALHOU ao remover o token
        de acesso. A rota deveria fazer a transição da dashboard para o
        login, mas está quebrando a aplicação.

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import AppRoutes from "./routes/routes";
+
+import GlobalStyle from "./styles/global";
+import { BaseContainer } from "./styles/Container";
+import NotficationProvider from "./contexts/NotificationContext";
+
+const App = () => {
+  return (
+    <BaseContainer>
+      <NotficationProvider>
+        <GlobalStyle />
+        <AppRoutes />
+      </NotficationProvider>
+    </BaseContainer>
+  );
+};
+
+export default App;
