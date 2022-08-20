96c0b9b69f275e36bc52a50fbc3c56d5d7cada43
Francisco Stenico <francisco.stenico@gmail.com>
2022-08-19 21:04:43 -0300

    Refact(typeScript): Tipagem dos Toasts

---
+++ src/components/ToastBox/index.tsx
@@ -0,0 +1,19 @@
+import { ToastThrower } from "./styles";
+
+const ToastBox = () => {
+  return (
+    <ToastThrower
+      position="top-center"
+      autoClose={4000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+    />
+  );
+};
+
+export default ToastBox;
